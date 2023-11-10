f60a2f0f7e3efabc0af4fd4817df12991302caf9
arkoa <arkoannuk+prog@gmail.com>
2023-11-10 19:18:13 +0200

    Add pages structure

---
+++ pages/rooms/holiday-home.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <p class="text-sm ...">
+    The quick brown duck ...
+  </p>
+  <p class="text-base ...">
+    The quick brown fox ...
+  </p>
+  <p class="text-lg ...">
+    The quick brown fox ...
+  </p>
+  <p class="text-xl ...">
+    The quick brown fox ...
+  </p>
+  <p class="text-2xl ...">
+    The quick brown fox ...
+  </p>
+  <div class="prose">
+    <h1>
+      Test
+    </h1>
+    <h2>
+      Test
+    </h2>
+    <h3>Test</h3>
+    <h4>Test</h4>
+    <p>Hey</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
