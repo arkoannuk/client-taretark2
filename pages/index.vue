61397422ae1b861d9656407e1889fc78c5439def
arkoa <arkoannuk+prog@gmail.com>
2023-11-10 15:34:44 +0200

    Add ESLint w/ antfu config

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+onMounted(() => {
+  const welcomeMessage = "Hey"
+  const additionalMessage = "Welcome to our component!"
+
+  if (welcomeMessage)
+    console.log(`${welcomeMessage} ${additionalMessage}`)
+  else
+    console.log("No welcome message available.")
+
+  // Additional logic can be added here
+})
+</script>
+
 <template>
   <div>
     <h2>Home</h2>
@@ -6,14 +20,14 @@
       dolorem debitis officia id harum temporibus sed, ex ut expedita.
     </p>
     <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo odit
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo oditasdd ddddd dddddddd
       dolorem debitis officia id harum temporibus sed, ex ut expedita.
     </p>
   </div>
 
-  <button class="btn btn-primary">Hello daisyUI</button>
+  <button class="btn btn-primary">
+    Hello daisyUI
+  </button>
 </template>
-
-<script setup lang="ts"></script>
 
 <style scoped></style>
